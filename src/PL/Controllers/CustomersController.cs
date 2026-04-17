2f3c5e9d164031b9a44f3e2514aba4f9821da6db
Mustafa <mustafawork911@gmail.com>
2026-04-17 21:57:15 +0200

    added CustomersController

---
+++ src/PL/Controllers/CustomersController.cs
@@ -0,0 +1,32 @@
+using BLL.DTOs.Customer;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomersController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CustomerDto>> GetById(int id)
+        {
+            var customer = await _customerService.GetByIdAsync(id);
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerDto>> Create([FromBody] CreateCustomerDto dto)
+        {
+            var customer = await _customerService.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetById), new { id = customer.Id }, customer);
+        }
+    }
+}
